29a5223835085d12bbac4169a51bcc432e318c63
NewFag <igorkorobov324@gmail.com>
2023-01-15 22:07:27 +0300

    add preform

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('test')
+import pygame
+
+if __name__ == '__main__':
+    pygame.init()
+    size = width, height = 500, 500
+    screen = pygame.display.set_mode(size)
+    screen.fill((0, 0, 0))
+    pygame.display.set_caption('Fair Final')
+    pygame.display.flip()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
