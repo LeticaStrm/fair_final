8e43d37dc7056be0075d78aae10002fa12f5aa97
NewFag <igorkorobov324@gmail.com>
2023-01-15 22:30:22 +0300

    add resize function

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 if __name__ == '__main__':
     pygame.init()
     size = width, height = 500, 500
-    screen = pygame.display.set_mode(size)
+    screen = pygame.display.set_mode(size, pygame.RESIZABLE)
     screen.fill((0, 0, 0))
     pygame.display.set_caption('Fair Final')
     pygame.display.flip()
@@ -12,3 +12,5 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        pygame.display.flip()
+        screen.fill((0, 0, 0))
