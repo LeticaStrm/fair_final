940ba809b6ccf1f0882ebb8b1350d1f30b8bbec4
NewFag <igorkorobov324@gmail.com>
2023-01-15 23:24:09 +0300

    add background

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from functions import load_image
+
 
 if __name__ == '__main__':
     pygame.init()
@@ -6,11 +8,15 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode(size, pygame.RESIZABLE)
     screen.fill((0, 0, 0))
     pygame.display.set_caption('Fair Final')
+    lvl1_bg = load_image('lvl1.jpg')
+    lvl1_bg = pygame.transform.scale(lvl1_bg, (pygame.display.get_window_size()))
     pygame.display.flip()
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        pygame.display.flip()
         screen.fill((0, 0, 0))
+        lvl1_bg = pygame.transform.scale(lvl1_bg, (pygame.display.get_window_size()))
+        screen.blit(lvl1_bg, (0, 0))
+        pygame.display.flip()
