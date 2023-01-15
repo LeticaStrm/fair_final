a0c842f06d41b98d7b27196d192343fbcf6c78b7
NewFag <igorkorobov324@gmail.com>
2023-01-15 22:43:15 +0300

    add load_images def

---
+++ functions.py
@@ -0,0 +1,12 @@
+import pygame
+import sys
+import os
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
